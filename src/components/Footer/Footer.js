0b5cc85a3deeb6bbee5253911f3f5c8ed0a94a4c
Teboho <tebohomphure@gmail.com>
2021-07-06 23:30:50 +0200

    add footer dynamic classes and favicon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return ( 
-        <div className="foot tc secondary-type">2018 - {new Date().getFullYear()} Teboho</div>
-     );
+
+    const [documentSize, setDocumentSize] = useState(0);
+    const [scrollSize, setScrollSize] = useState(0);
+
+    useEffect(() => {
+        setScrollSize(document.body.scrollHeight);
+        setDocumentSize(document.documentElement.clientHeight);
+    });
+
+    
+    return (
+        <div className={`foot tc secondary-type ${scrollSize > documentSize ? '' : 'foot-fit-to-screen'}`}>2018 - {new Date().getFullYear()} Teboho</div>
+    );
 }
- 
+
 export default Footer;
