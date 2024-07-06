571fd7c2245669a8d2414f1fc6bb2ab3f6ba35df
Teboho <tebohomphure@gmail.com>
2024-07-06 19:45:20 +0200

    correct typo

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import './Intro.css';
-import '../../styles/global.css';
-import { navigate } from 'gatsby';
+import React from "react"
+import "./Intro.css"
+import "../../styles/global.css"
+import { navigate } from "gatsby"
 
 const Intro = () => {
-    return ( 
-        <div className="container tc">
-          <div className="intro-cta">
-            <img className="profile-image" src="/UNADJUSTEDNONRAW_thumb_87.jpg" alt="self portrait"/>
-            <div className="tm"><strong>Howdy, I'm Teboho</strong></div>
-            <div className="txl"><strong>Builder of Software and Great Digital Experiences.</strong></div>
-            <p>I am a <strong>Fullstack Software Developer</strong> fascinated by the modern web and thrilled by building software from ideas to end product.</p>
+	return (
+		<div className="container tc">
+			<div className="intro-cta">
+				<img
+					className="profile-image"
+					src="/UNADJUSTEDNONRAW_thumb_87.jpg"
+					alt="self portrait"
+				/>
+				<div className="tm">
+					<strong>Howdy, I'm Teboho</strong>
+				</div>
+				<div className="txl">
+					<strong>
+						Builder of Software and Great Digital Experiences.
+					</strong>
+				</div>
+				<p>
+					I am a <strong>Fullstack Software Developer</strong>{" "}
+					fascinated by the modern web and thrilled by building
+					software from idea to end product.
+				</p>
 
-            <button className="button-primary" onClick={() => navigate('/contact')}>SAY HI</button>
-          </div>
-        </div>
-     );
+				<button
+					className="button-primary"
+					onClick={() => navigate("/contact")}
+				>
+					SAY HI
+				</button>
+			</div>
+		</div>
+	)
 }
- 
-export default Intro;
+
+export default Intro
