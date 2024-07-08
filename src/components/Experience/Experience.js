ee5905c8feacf0aa50508c67878ef455b4b44264
Teboho <tebohomphure@gmail.com>
2024-07-08 21:58:47 +0200

    update minor issues

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import './Experience.css';
-import '../../styles/global.css';
+import React from "react"
+import "./Experience.css"
+import "../../styles/global.css"
 
 const Experience = () => {
-    return ( 
-        <div id="experience" className="experience container tc">
-            <h1 class="experience__title">Experience</h1>
-            <div className="experience__items">
-                <div className="experience__item tl">
-                    <div className="company">Entelect</div>
-                    <div className="role">Software Engineer</div>
-                    <div className="duration">Aug 2019 - Current</div>
-                </div>
-                <div className="experience__item tl">
-                    <div className="company">PSG Wealth</div>
-                    <div className="role">Junior Developer</div>
-                    <div className="duration">Oct 2018 - Jul 2019</div>
-                </div>
-                <div className="experience__item tl">
-                    <div className="company">EOH</div>
-                    <div className="role">Analyst Consultant</div>
-                    <div className="duration">May 2015 - Jun 2018</div>
-                </div>
-                
-            </div>
-            
-        </div>
-     );
+	return (
+		<div id="experience" className="experience container tc">
+			<h1 className="experience__title">Experience</h1>
+			<div className="experience__items">
+				<div className="experience__item tl">
+					<div className="company">Entelect</div>
+					<div className="role">Software Engineer</div>
+					<div className="duration">Aug 2019 - Current</div>
+				</div>
+				<div className="experience__item tl">
+					<div className="company">PSG Wealth</div>
+					<div className="role">Junior Developer</div>
+					<div className="duration">Oct 2018 - Jul 2019</div>
+				</div>
+				<div className="experience__item tl">
+					<div className="company">EOH</div>
+					<div className="role">Analyst Consultant</div>
+					<div className="duration">May 2015 - Jun 2018</div>
+				</div>
+			</div>
+		</div>
+	)
 }
- 
-export default Experience;
+
+export default Experience
