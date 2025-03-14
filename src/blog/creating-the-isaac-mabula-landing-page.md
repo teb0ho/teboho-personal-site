debf2c43cc3333a2e17b257302efa289d66c8488
Teboho Mphure <tebohomphure@gmail.com>
2025-03-14 22:28:21 +0200

    add portfolio and blog post

---
+++ src/blog/creating-the-isaac-mabula-landing-page.md
@@ -0,0 +1,44 @@
+---
+title: Redesigning my Personal Website 🖌🎨
+slug: redesigning-my-personal-website
+date: 2021-08-14
+author: Teboho
+image: ../images/photo-1517697471339-4aa32003c11a.jpg
+---
+
+After a few years of my personal website's existence I decided it was time to give it an update and I went ahead and did that.
+
+This is how the website looked before and after the update.
+
+My website before the update.
+![Old website](../images/portfolio-site-old.png)
+
+My website after the update.
+![New website](../images/portfolio-site-new.png)
+
+The changes I made to the new website are as follows.
+
+1. **Booting out bootstrap**
+1. **Added blog functionality**
+1. **Light mode and dark mode themes**
+1. **Simplified design**
+
+My reasons for the above are as follows
+
+### Booting out bootstrap
+
+On the old website I was using bootstrap, particularly it's grid system and the mobile first navbar. I feel like the parts of bootstrap I was using on the site were too small to justify adding an entire bootsrap library on this new site, so I rather rewrote the parts I used by using plain css.
+
+### Added blog functionality
+
+On the new website I also wanted to add a blog, so I chose [gatsby js](https://www.gatsbyjs.com/) in order to do that. The website was previously a plain JS, HTML & CSS project.
+
+### Light mode and dark mode themes
+
+I have added a light and a dark theme, the website will adapt and match the theme you are using on your device (computer or mobile).
+
+### Simplified design
+
+I'm not a design expert but on this new website I tried to use less fonts and I decided to drop the background imagery I was using.
+
+That's all for now see you on the next post.
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,62 @@
-import React, { useState } from 'react';
-import '@fontsource/varela-round';
-import './Navbar.css';
-
-
+import React, { useState } from "react"
+import "@fontsource/varela-round"
+import "./Navbar.css"
 
 const NavBar = () => {
-    const [open, setOpen] = useState('');
+  const [open, setOpen] = useState("")
 
-    const toggleMenu = () => {
-        if (open === '') {
-            setOpen('open');
-        }
-        else {
-            setOpen('');
-        }
+  const toggleMenu = () => {
+    if (open === "") {
+      setOpen("open")
+    } else {
+      setOpen("")
     }
+  }
 
-    const handleNavClick = () => {
-        setOpen('');
-    }
+  const handleNavClick = () => {
+    setOpen("")
+  }
 
-    return (
-        <>
-            <div className="overlay"></div>
-            <div className={`menu ${open}`}>
-                    <a href="/">Home</a>
-                    <a onClick={handleNavClick} href="/blog">Blog</a>
-                    <a onClick={handleNavClick} href="/contact">Contact</a>
-
-            </div>
-            <nav>
-                <a href="/" aria-label="Landing page link" className="logo"><span ><strong>teboho<span className="surname">mphure</span></strong></span></a>
-                <div role="menu" tabIndex={0} onClick={toggleMenu} onKeyDown={toggleMenu} className={`hamburger ${open}`}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <div className="header__links">
-                    <a href="/">Home</a>
-                    <a href="/blog">Blog</a>
-                    <a href="/contact">Contact</a>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className={`menu ${open}`}>
+        <a href="/">Home</a>
+        <a onClick={handleNavClick} href="/blog">
+          Blog
+        </a>
+        <a onClick={handleNavClick} href="/contact">
+          Contact
+        </a>
+      </div>
+      <nav>
+        <a href="/" aria-label="Landing page link" className="logo">
+          <span>
+            <strong>
+              teboho<span className="surname">mphure</span>
+            </strong>
+          </span>
+        </a>
+        <div
+          role="menu"
+          tabIndex={0}
+          onClick={toggleMenu}
+          onKeyDown={toggleMenu}
+          className={`hamburger ${open}`}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="header__links">
+          <a href="/">Home</a>
+          <a href="/portfolio">Portfolio</a>
+          <a href="/blog">Blog</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </nav>
+    </>
+  )
 }
 
-export default NavBar;
+export default NavBar
